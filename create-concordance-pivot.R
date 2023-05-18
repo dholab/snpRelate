de318f7a218ec19ec7d9ee871a51da0d7ac4dcaf
Nick Minor <74076334+nrminor@users.noreply.github.com>
2023-05-18 09:03:44 -0500

    Add files via upload

---
+++ create-concordance-pivot.R
@@ -0,0 +1,75 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# load necessary packages
+library(tidyverse)
+library(readxl)
+library(writexl)
+
+# parse command line arguments
+cycle_table = args[1]
+
+# create a vector of detailed report files
+results_files <- list.files(path = ".", pattern = "_Detailed_Results")
+
+# read in optimal cycle table
+cycle_table <- read.csv(cycle_table)
+
+# Read in the list of SNPs
+SNPs <- read_excel(results_files[1], sheet = 1, skip = 15)[1] %>%
+  separate(ID, into = c("sample", "SNP"), sep = "-") %>%
+  subset(select = "SNP")
+
+# loop through each SNP to collate calls
+concordance_table <- data.frame("SNP" = sort(unique(SNPs$SNP)),	
+                                "Call_Map_Order" = NA,
+                                "Cycle" = NA, 
+                                "Comments" = NA)
+for (snp in concordance_table$SNP){
+  
+  # determine optimal cycle
+  optimal_cycle <- as.integer(cycle_table[cycle_table$SNP==snp,"cycle"])
+  
+  # read in the correct BioMark output file based on that cycle
+  data <- read_excel(paste("28637_BMX003_Detailed_Results_cycle", optimal_cycle, ".xlsx", sep = ""), 
+                     sheet = 1, skip = 15)
+  
+  # split sample IDs and SNP IDs into their own columns
+  data <- separate(data, ID, into = c("sample", "SNP"), sep = "-")
+  
+  snp_table <- data[data$SNP==snp,]
+  new_index <- which(concordance_table$SNP==snp)
+  
+  concordance_table[new_index, "Call_Map_Order"] <- unique(snp_table$SNP)
+  concordance_table[new_index, "Cycle"] <- optimal_cycle
+  
+  if (snp == concordance_table$SNP[1]) {
+
+    for (animal in sort(unique(snp_table$Name))){
+
+      call <- snp_table[snp_table$Name==animal,"Final"][1,] %>%
+        as.character()
+      concordance_table <- cbind(concordance_table, as.data.frame(NA))
+      concordance_table[new_index,ncol(concordance_table)] <- call
+      colnames(concordance_table)[ncol(concordance_table)] <- animal
+
+    }
+
+  } else if (ncol(concordance_table[,5:ncol(concordance_table)]) != length(unique(data$sample))+1) {
+    
+    for (animal in sort(unique(snp_table$Name))){
+      
+      call <- snp_table[snp_table$Name==animal,"Final"][1,] %>%
+        as.character()
+      concordance_table[new_index,animal] <- call
+      
+    }
+    
+  } else {
+    break
+  }
+  
+}
+
+# write output file for viewing in excel
+write_xlsx(concordance_table, "concordance-table.xlsx")
---
+++ select-optimal-cycle.R
@@ -0,0 +1,116 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# Load required libraries
+library(tidyverse)
+library(ggplot2)
+library(cluster)
+library(MASS)
+library(ellipse)
+library(readxl)
+library(writexl)
+
+# create a vector of detailed report files
+results_files <- list.files(path = ".", pattern = "_Detailed_Results")
+
+# create a data frame of optimal cycles for all SNPs
+cycles <- data.frame("SNP" = sort(unique(data$SNP)),
+                     "cycle" = as.character(NA))
+
+# make a data frame that records best cycles for each assay
+best_cycles <- data.frame("SNP" = as.character(NA),
+                          "distance" = as.numeric(0),
+                          "cycle" = as.numeric(0))
+best_cycles <- best_cycles[NULL,]
+
+# create a loop that goes through each result file
+for (i in results_files){
+  
+  # define index
+  index <- which(results_files==i)
+  
+  # define cycle based on file name
+  cycle <- 7
+  centroid_distances[index, "cycle"] <- cycle
+  
+  # Read in the data
+  data <- read_excel(i, sheet = 1, skip = 15)
+  
+  # reduce to the columns of interest
+  data <- data.frame("cycle" = cycle,
+                     "ID" = data$ID,
+                     "call" = data$Final,
+                     "X.value" = data$`Allele X...11`,
+                     "Y.value" = data$`Allele Y...12`)
+  
+  # split ID column
+  data <- separate(data, ID, into = c("sample", "SNP"), sep = "-")
+  
+  # remove no-calls and NTCs
+  data <- data[data$call!="No Call" &
+                 data$call!="NTC",] ; rownames(data) <- NULL
+  
+  # loop through each SNP
+  for (snp in sort(unique(data$SNP))){
+    
+    # add new row to best_cycles data frame
+    if (!(snp %in% best_cycles$SNP)){
+      best_cycles <- rbind(best_cycles, c(snp, 0, 0))
+      colnames(best_cycles) <- c("SNP","distance","cycle")
+    }
+    
+    snp_table <- data[data$SNP==snp,]
+    
+    # Fit ellipses and find centroids
+    centroids <- data.frame(Zygosity = c("XX", "XY", "YY"), X = numeric(3), Y = numeric(3))
+    for (zyg in c("XX", "XY", "YY")) {
+      # Subset the data by Zygosity
+      subset_data <- snp_table[snp_table$call == zyg, ]
+      
+      if (nrow(subset_data) > 1){
+        
+        # Calculate the ellipse
+        ell <- cov.trob(subset_data[, c("X.value", "Y.value")])
+        
+        # Calculate the centroid
+        centroids[centroids$Zygosity == zyg, c("X", "Y")] <- ell$center
+        
+        if (nrow(centroids)>1){
+          # Calculate pairwise distances
+          distances <- dist(centroids[, c("X", "Y")])
+        }
+        
+      } else if (nrow(subset_data) == 0){
+        
+        centroids <- centroids[!centroids$Zygosity == zyg,]
+        
+      } else {
+        
+        centroids[centroids$Zygosity == zyg, c("X", "Y")] <- subset_data[, c("X.value", "Y.value")]
+        
+        if (nrow(centroids)>1){
+          # Calculate pairwise distances
+          distances <- dist(centroids[, c("X", "Y")])
+        }
+        
+      }
+      
+      if (nrow(centroids)==0){
+        distances = c(0,0)
+      }
+      
+      # determine if this is the best-yet cycle for this SNP
+      if (mean(distances) > best_cycles[best_cycles$SNP==snp, "distance"] 
+          | is.null(best_cycles[best_cycles$SNP==snp, "distance"])){
+        best_cycles[best_cycles$SNP==snp, "distance"] <- mean(distances)
+        best_cycles[best_cycles$SNP==snp, "cycle"] <- cycle
+      }
+      
+    }
+    
+  }
+  
+}
+
+# output file
+write.csv(best_cycles, file = "optimal-cycles.csv")
